fd998315e6c7554ff7fc646ca1ba652cc08c8d2c
crui14994 <778943319@qq.com>
2019-08-14 16:44:09 +0800

    修复添加css前缀bug

--- build/webpack.rules.conf.js
+++ build/webpack.rules.conf.js
@@ -1,12 +1,12 @@
 //分离css，webpack4推荐的分离css的插件
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const autoprefixer = require('autoprefixer');//给css自动加浏览器兼容性前缀的插件
+
 const rules = [
-    
+
     {
         test: /\.js$/,
         exclude: /(node_modules)/,
-        include: /src/,
+        include: /src/, //限制范围，提高打包速度
         use: [
             {
                 loader: 'babel-loader',
@@ -18,17 +18,21 @@ const rules = [
         ]
     },
     {
-        test:/\.(css|scss|sass)$/,
-        use: process.env.NODE_ENV === "development" ? ["style-loader", "css-loader", "sass-loader", "postcss-loader"] :[MiniCssExtractPlugin.loader,"css-loader",{
-            loader: "postcss-loader",
-            options: {
-                plugins: [
-                    autoprefixer({
-                        browsers: ['ie >= 8','Firefox >= 20', 'Safari >= 5', 'Android >= 4','Ios >= 6', 'last 4 version']
-                    })
-                ]
-            }
-        },"sass-loader"]
+        test: /\.(css|scss|sass)$/,
+        // use: process.env.NODE_ENV === "development" ? ["style-loader", "css-loader", "sass-loader", "postcss-loader"] :[MiniCssExtractPlugin.loader,"css-loader",{
+        //     loader: "postcss-loader",
+        //     options: {
+        //         plugins: [
+        //             autoprefixer({
+        //                 browsers: ['ie >= 8','Firefox >= 20', 'Safari >= 5', 'Android >= 4','Ios >= 6', 'last 4 version']
+        //             })
+        //         ]
+        //     }
+        // },"sass-loader"]
+        use: process.env.NODE_ENV === "development" ? ["style-loader", "css-loader", "sass-loader", "postcss-loader"] :
+            [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader", "sass-loader"],
+        include: /src/, //限制范围，提高打包速度
+        exclude: /node_modules/
     }
 
 ];
--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,5 @@
-// module.exports = {
-//     plugins: [
-//         require('autoprefixer')({
-//             "browsers": [
-//                 "defaults",
-//                 "> 1%",
-//                 "last 2 versions",
-//                 "not ie <= 8",
-//                 "ios > 7"
-//             ]
-//         })
-//     ]
-// };
+module.exports = {
+    plugins: [
+        require('autoprefixer')
+    ]
+};
